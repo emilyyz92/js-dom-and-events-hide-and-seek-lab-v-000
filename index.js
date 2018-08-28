20e9cf39239de50dc426b0097c06f3603beda4a1
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-28 17:48:19 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+const getFirstSelector = function (selector) {
+  return document.querySelector(selector)
+}
+
+const nestedTarget = function () {
+  return document.querySelector('#nested .target')
+}
+
+const deepestChild = function() {
+  return document.querySelector('#grand-node div div div div')
+}
+
+const increaseRankBy = function(n) {
+  const list = document.querySelectorAll(`#app .ranked-list li`)
+  for (let i=0; i< list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + parseInt(n)
+  }
+  return list
+}
